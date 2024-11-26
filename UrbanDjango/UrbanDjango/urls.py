13d643cc2337d2211b956cf89befc32b0d87b03a
Art21042147 <art21042147@gmail.com>
2024-11-26 11:47:52 +0300

    DTL. Теги, наследование.

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from task2.views import ClassTemplate, func_template
-from task3.views import *
+# from task3.views import *
+from task4.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
---
+++ UrbanDjango/task4/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+def main_page(request):
+    title = "Main Page"
+    main_head = "Game Booster"
+    second_head = "Let's play!"
+    shop = "Shop"
+    cart = "Cart"
+    context = {
+        'title': title,
+        'main_head': main_head,
+        'second_head': second_head,
+        'shop': shop,
+        'cart': cart,
+    }
+    return render(request, 'fourth_task/platform.html', context)
+
+
+class ShopView(TemplateView):
+    template_name = 'fourth_task/games.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Shop'
+        context['head'] = 'New Games'
+        context['buy'] = 'Buy'
+        context['return'] = 'Return to home page'
+        context['games'] = ['Metal Beer Liquid',
+                            'Tomb Provider',
+                            'Resident Liver']
+        return context
+
+
+class CartView(TemplateView):
+    template_name = 'fourth_task/cart.html'
